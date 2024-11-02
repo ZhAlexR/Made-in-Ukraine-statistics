0ce1277aa05de8b6821243336ded7c88caadb44c
ZhAlexR <oleksandrzhukov95@gmail.com>
2024-11-02 18:33:50 +0200

    Updated links

--- data_parser/main.py
+++ data_parser/main.py
@@ -1,19 +1,28 @@
 import asyncio
+import logging
 from pathlib import Path
 
 from scraper import get_files
 
 urls = [
-    "https://madeinukraine.gov.ua/files/perelik-tovariv/products.csv",
-    "https://madeinukraine.gov.ua/files/perelik-prodavtsiv/perelik-prodavtsiv.csv"
+    "https://api.madeinukraine.gov.ua/storage/exports/products.csv",
+    "https://api.madeinukraine.gov.ua/storage/exports/perelik-prodavtsiv.csv"
 ]
 
 files_name = ["products", "sellers"]
 CURRENT_DIR = Path.cwd()
 
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    handlers=[logging.StreamHandler()]
+)
+logger = logging.getLogger(__name__)
 
 async def main():
+    logger.info("Starting parse files ...")
     await get_files(urls=urls, names=files_name)
+    logger.info("Stopping parse files ...")
 
 if __name__ == "__main__":
     asyncio.run(main())
